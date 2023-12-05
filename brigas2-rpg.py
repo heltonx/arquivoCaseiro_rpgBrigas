5e433f4dcde83079c1891e8002329e89964e831a
Helton Xavier Hernandez <52825808+heltonx@users.noreply.github.com>
2023-12-05 15:31:44 -0300

    Add files via upload

---
+++ brigas2-rpg.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random
+
+class GloryTender:
+	__doc__="""Classe dos heróis hernandinos"""
+
+	nome=''
+	golpe=''
+	poder=0
+	localizacao=''
+
+	def __init__(self,nome,golpe,poder,localizacao):
+		__doc__="""vamos declarar essa bosta????"""
+		self.nome = nome
+		self.golpe = golpe
+		self.poder = poder
+		self.localizacao= localizacao
+
+	def ataca_hitler(self):
+		__doc__="""Nosso herói atacará o Adolfo"""
+		print(self.nome+"atacou o hitler com um"+self.golpe)
+		
+	def usa_arma(self):
+		x = input("escolha uma instancia da arma que tu queres: ")
+		print(self.nome + " usou " + x.nome +" e "+x.tiro_alvo())
+                
+class Arma:
+	__doc__="""Ferramentas e instrumentos de guerra pros nossos heróis sairem na porrada e destruirem o capeta"""
+	nome=''
+	poder=0
+	
+	def __init__(self,nome,poder):
+		self.nome = nome
+		self.poder = poder
+
+	def tiro_alvo(self):
+
+		sort = random.randint(1,10)
+
+		if sort == 1:
+			return ('atirou no chao!!!')
+		elif sort ==2:
+			return ('atirou dez metros acima do alvo')
+		elif sort ==3:
+			return ('acertou no alvo, mas na borda')
+		elif sort ==4:
+			return ('acertou quase no meio, eh um mestre!')
+		elif sort ==5:
+			return ('acertou no meio, voce estah pronto pra enfrentar a guerra!!!')
+		else:
+			return ('a arma nao funcionou!!!')
+
+
+class Iterations:
+	__doc__="""coloque dois objetos jogador aqui e 'bote as aranha pra brigá' """
+	jogador1=''
+	jogador2=''
+
+	def __init__(self,jogador1,jogador2):
+		self.jogador1=jogador1
+		self.jogador2=jogador2
+  
+	def briga(self):
+		print(self.jogador1.nome," deu um pontepé na bundo do ",self.jogador2.nome,", deixando ",self.jogador2.poder,"cm de cu aberto")
+
+	def compara(self):
+		if self.jogador1.poder > self.jogador2.poder:
+			print(self.jogador1.nome+"é mais forte!")
+		else:
+			print(self.jogador2.nome+"é mais forte!")
+
+#Fim do programa
+
+olavo = GloryTender('Olavo de Carvalho ',' vai tomar no cu!',6,'Virgínia')
+rubens = GloryTender('Irmão Rubens','Seus cães gulosos!',3,'Fluminência')
+pinto = GloryTender('Paulo Sérgio Pinto','Estamos encerrando Pampa Debates',4,'Alto Guaíba')
+
+pistola = Arma("Pistola Calibre 38",4)
+ak = Arma('AK-47',7)
+espada = Arma('Espada',3)
